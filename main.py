0c8f8b8ec0c048aa9668cbf2fd168b7e15ef7a53
Joni Moas <setpsistariasbaniou@gmail.com>
2020-10-08 01:03:30 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from flask import Flask
+from flask import request
+from flask import Response
+from flask import jsonify
+import urllib.parse
+import json
+import datetime
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
+app = Flask(__name__)
+
+
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def main(path):
+    method = request.method
+    args = request.args.to_dict(flat=False)
+    headers = dict(request.headers)
+    body = request.get_json()
+    response = requests.request(method, os.environ["API_TO_WATCH"]+"users", params=args, stream=True,
+                                headers=headers, allow_redirects=False, data=json.dumps(body))
+    response_body = None
+    response_headers = dict(response.headers)
+    try:
+        response_body = response.json()
+    except:
+        response_body = None
+    db.insert(dict({'method': request.method,
+                    'path': str(path),
+                    'args': args,
+                    'request_headers': headers,
+                    'request_body': body,
+                    'response': response_body,
+                    'response_headers': response_headers,
+                    'timestamp': str(datetime.datetime.now()),
+                    'status': response.status_code
+                    }))
+    result = None
+    if response_body != None:
+        result = jsonify(response_body)
+    else:
+        result = Response()
+    result.status_code = response.status_code
+    result.headers = response_headers
+    return result
---
+++ run.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=main.py
+set API_TO_WATCH=http://localhost:3001/
+flask run
